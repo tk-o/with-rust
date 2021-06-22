bd450b8c823609334b090b2ca961e15207603ded
Tomasz Kopacki <tomasz@kopacki.net>
2021-06-22 21:52:10 +1000

    fix(cli-101): import name

--- cli-101/src/main.rs
+++ cli-101/src/main.rs
@@ -1,4 +1,4 @@
-use marketpalce;
+use marketplace;
 /// This program enables sellers to put their offers on a marketplace,
 /// and also make it possible for buyers to purcharse a product/service on this marketpalce.
 fn main() {
