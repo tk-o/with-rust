ab361bcd0f68ab52bf4c67868f1e5eb17ae4e298
Tomasz Kopacki <tomasz@kopacki.net>
2021-07-04 21:54:52 +1000

    feat(re-encryption): start example
    The example aims to encrpypt a message, and then make it available for each subscriber to decrypt the message using their own private key. The proxy re-encryption is required here to wrap the symmetric key the message was encrypted with and distribute it to subscribers in capsulas that can only be opened with their private keys.

--- keyrings/src/lib.rs
+++ keyrings/src/lib.rs
@@ -1,10 +1,35 @@
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn it_works() {
+        use recrypt::api::Plaintext;
+        use aes_gcm::{Aes256Gcm, Key, Nonce};
+        use aes_gcm::aead::{Aead, NewAead};
         use recrypt::prelude::*;
+        use std::fs;
 
-// create a new recrypt
+        // START: data encryption
+        // load data to that is going to be shared as a message
+        let message = fs::read_to_string("input/raw-data-provided-via-subscription.json")
+            .expect("A file with raw data must exist");
+
+        let key = Key::from_slice(b"an example very very secret key.");
+        let cipher = Aes256Gcm::new(key);
+
+        let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
+
+        let ciphertext = cipher.encrypt(nonce, message.as_bytes().as_ref())
+            .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
+
+        let plaintext = cipher.decrypt(nonce, ciphertext.as_ref())
+            .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
+
+        assert_eq!(&plaintext, message.as_bytes());
+        // END: data encryption
+
+        // START: key encryption
+
         let mut recrypt = Recrypt::new();
 
 // generate a plaintext to encrypt
@@ -41,6 +66,7 @@ mod tests {
 
 // plaintext recovered.
         assert_eq!(pt, decrypted_val);
+
 
     }
 }
