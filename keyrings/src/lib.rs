8c9db83fc199a024033be6b011c98b8d684c1281
Tomasz Kopacki <tomasz@kopacki.net>
2021-07-06 21:04:33 +1000

    chore(pre-scheme): update the example
    Proxy re-encryption scheme is quite complicated. So far I've got a piece of code to encrypt/decrypt data, using AES algorithm. I've also got the data encryption key part to work, that enables transforming public key attached to the encrypted data. I will need to connect these two pieces together.

--- keyrings/src/lib.rs
+++ keyrings/src/lib.rs
@@ -1,6 +1,5 @@
 #[cfg(test)]
 mod tests {
-
     #[test]
     fn it_works() {
         use recrypt::api::Plaintext;
@@ -14,59 +13,77 @@ mod tests {
         let message = fs::read_to_string("input/raw-data-provided-via-subscription.json")
             .expect("A file with raw data must exist");
 
-        let key = Key::from_slice(b"an example very very secret key.");
-        let cipher = Aes256Gcm::new(key);
 
+        // generate Data Encryption Key (DEK)
+        let dek = Key::from_slice(b"an example very very secret key.");
+        // encrypt data
+        let cipher = Aes256Gcm::new(dek);
         let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
 
         let ciphertext = cipher.encrypt(nonce, message.as_bytes().as_ref())
             .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
 
+        // validate the AES implementation
         let plaintext = cipher.decrypt(nonce, ciphertext.as_ref())
             .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
 
-        assert_eq!(&plaintext, message.as_bytes());
+        assert_eq!(String::from_utf8(plaintext).unwrap(), message);
         // END: data encryption
 
-        // START: key encryption
+        // START: DEK encryption
 
         let mut recrypt = Recrypt::new();
 
-// generate a plaintext to encrypt
+        println!("DEK (len: {:?}): {:?}", dek.len(), dek);
+        // generate a plaintext to encrypt
+        // it's going to represent a message `m`
+        // TODO: make sure to pass both: the DEK and the nonce here
+        //       as these are needed to decrypt AES-ecrypted data
         let pt = recrypt.gen_plaintext();
 
-// generate signing keys
-        let signing_keypair= recrypt.generate_ed25519_key_pair();
+        // ALICE: generates a public/private keypair to encrypt the message `m` initially
+        let (alice_priv_key, alice_pub_key) = recrypt.generate_key_pair().unwrap();
 
-// generate a public/private keypair to encrypt the data to initially.
-        let (initial_priv_key, initial_pub_key) = recrypt.generate_key_pair().unwrap();
+        // ENRICO: generate signing keys
+        let enrico_keypair = recrypt.generate_ed25519_key_pair();
 
-// encrypt the data to `initial_pub_key`!
-        let encrypted_val = recrypt.encrypt(&pt, &initial_pub_key, &signing_keypair).unwrap();
+        // Enrico encrypts a message `m` to `pk_A`
+        // c_A = encrypt(pk_A, m)
+        // where the Enrico's key pair sits then?
+        let encrypted_val = recrypt.encrypt(&pt, &alice_pub_key, &enrico_keypair).unwrap();
 
-// generate a second public/private keypair as the target of the transform.
-// after applying the transform, `target_priv_key` will be able to decrypt the data!
-        let (target_priv_key, target_pub_key) = recrypt.generate_key_pair().unwrap();
+        // BOB: generate a second public/private keypair as the target of the transform.
+        // after applying the transform, `sk_B` will be able to decrypt the data!
+        let (bob_priv_key, bob_pub_key) = recrypt.generate_key_pair().unwrap();
 
-// generate a transform key that will change which private key can decrypt the data
-        let initial_to_target_transform_key = recrypt.generate_transform_key(
-            &initial_priv_key,
-            &target_pub_key,
-            &signing_keypair).unwrap();
+        // Alice generates a transform key that will change which private key can decrypt the data
+        // rk_A->B
+        let alice_to_bob_transform_key = recrypt.generate_transform_key(
+            &alice_priv_key,
+            &bob_pub_key,
+            &enrico_keypair
+        ).unwrap();
 
-// Transform the plaintext to be encrypted to the target!
-// The data is _not_ decrypted here. Simply transformed!
+        // URSULA: Transform the plaintext to be encrypted to the target!
+        // The data is _not_ decrypted here. Simply transformed!
+        // c_B = re-encrypt(rk_A->B, c_A)
         let transformed_val = recrypt.transform(
             encrypted_val,
-            initial_to_target_transform_key,
-            &signing_keypair).unwrap();
+            alice_to_bob_transform_key,
+            &enrico_keypair
+        ).unwrap();
 
-// decrypt the transformed value with the target private key and recover the plaintext
-        let decrypted_val = recrypt.decrypt(transformed_val, &target_priv_key).unwrap();
+        // TODO: understand how does Urusla know the signing keypair of Enrico? I'd mean Enrico revealing his private key!
+        // ???????
 
-// plaintext recovered.
+        // Bob decrypts the transformed value with the his `pk_B` and gets the message `m`
+        // m = decrypt(sk_B, c_B)
+        let decrypted_val = recrypt.decrypt(
+            transformed_val,
+            &bob_priv_key
+        ).unwrap();
+
+        // original message `m` recovered.
         assert_eq!(pt, decrypted_val);
-
-
     }
 }
