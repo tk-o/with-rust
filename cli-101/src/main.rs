62d267ed2a86f6f76138141693d98c1f7e869293
Tomasz Kopacki <tomasz@kopacki.net>
2021-07-27 20:15:33 +1000

    feat(cli): path + file ops
    Practice Path and File operations by reading file based on CWD, and by writing file to an output path read from input settings

--- cli-101/src/main.rs
+++ cli-101/src/main.rs
@@ -1,8 +1,44 @@
-use marketplace;
-/// This program enables sellers to put their offers on a marketplace,
-/// and also make it possible for buyers to purcharse a product/service on this marketpalce.
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
+use std::{io::prelude::*, env, path::Path, fs::{create_dir,File}};
+use serde::{Deserialize,Serialize};
+use serde_json;
 
-    println!("Hello, Mr {:?}, greetings from version: {:?}", args, marketplace::version());
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let cwd = env::current_dir().expect("cwd is defined");
+
+    let settings_path = cwd.join(&"input/settings.json");
+
+    let mut settings = String::new();
+
+    let mut settings_file = File::open(&settings_path).expect("Load settings file");
+
+    settings_file.read_to_string(&mut settings);
+
+    let settings: Settings = serde_json::from_str(&settings).unwrap();
+
+    println!("Hello, Mr {:?}, greetings from version: {:?}", settings_path, &settings);
+
+    let cwd = env::current_dir().expect("cwd is defined");
+    let output_path = cwd.join(&settings.output.path);
+
+    if !&output_path.exists() {
+        create_dir(&output_path);
+    }
+
+    let mut output_file = File::create(
+        cwd.join(&settings.output.path).join("result.md")
+    ).expect("Output file is created");
+
+    output_file.write_all(b"# my two bytes");
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Settings {
+    output: SettingsOutput,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SettingsOutput {
+    path: String,
 }
